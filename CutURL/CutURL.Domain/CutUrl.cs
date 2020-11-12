05ba68cc9dfc9e34e316af6d97f30f8621dd6564
Iamposox <60770099+Iamposox@users.noreply.github.com>
2020-11-12 14:19:50 +0400

    Add files via upload

---
+++ CutURL/CutURL.Domain/CutUrl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace CutURL.Domain
+{
+    public class CutUrl
+    {
+        public string[] GetShortURL(string[] longURL)
+        {
+            try
+            {
+                string[] shortURL = new string[longURL.Length];
+                for (int i = 0; i < longURL.Length; i++)
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        shortURL[i] = wc.DownloadString("https://clck.ru/--?url=" + longURL[i]);
+                    }
+                }
+                return shortURL;
+            }
+            catch(WebException wex)
+            {
+                Console.WriteLine(wex.InnerException.Message);
+                return Array.Empty<String>();
+            }
+        }
+        public string[] GetURLOnFile(string filename) => File.ReadAllLines(filename);
+    }
+}
