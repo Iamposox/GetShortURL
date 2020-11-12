7e34d32adbd75e6132e98ebeee9a03dd6d2421f6
Iamposox <60770099+Iamposox@users.noreply.github.com>
2020-11-12 14:22:08 +0400

    Add files via upload

---
+++ CutURL/ShortURL/Program.cs
@@ -0,0 +1,19 @@
+﻿using CutURL.Domain;
+using System;
+
+namespace ShortURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CutUrl cutUrl = new CutUrl();
+            var getUrlOnFile = cutUrl.GetURLOnFile("URL.txt");
+            var shortUrl = cutUrl.GetShortURL(getUrlOnFile);
+            for(int i=0; i< shortUrl.Length;i++)
+            {
+                Console.WriteLine($"{getUrlOnFile[i]} => {shortUrl[i]}");
+            }
+        }
+    }
+}
