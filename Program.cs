9d16ec9a1e90610163ce4ddc3167213285382084
Iamposox <60770099+Iamposox@users.noreply.github.com>
2020-11-20 15:25:23 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using CutURL.Domain;
+using System;
+
+namespace ShortURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CutUrl cutUrl = new CutUrl();
+            var getUrlOnFile = cutUrl.GetURLOnFile("URL.txt");
+            var shortUrl = cutUrl.GetShortURL(getUrlOnFile);
+            for(int i=0; i< shortUrl.Length;i++)
+            {
+                Console.WriteLine($"{getUrlOnFile[i]} => {shortUrl[i]}");
+            }
+            Console.WriteLine("После нажатия Enter консоль закроется");
+            Console.ReadLine();
+        }
+    }
+}
